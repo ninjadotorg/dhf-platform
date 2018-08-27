f9c77edc54a75cad312fdd4632295ea648bea2eb
blackbean <tuananh@autonomous.nyc>
2018-08-27 12:17:42 +0700

    create project model

--- dhf_backend/common/models/account.js
+++ dhf_backend/common/models/account.js
@@ -1,27 +1,9 @@
 'use strict';
 module.exports = function(Account) {
-  Account.validatesInclusionOf('userType', {
-    in: ['admin', 'trader', 'user'],
-    message: 'must be trader or user',
-  });
   Account.observe('after save', function(ctx, next) {
     next();
   });
   Account.afterRemote('create', function(context, user, next) {
-    /*
-    add user to role
-    * */
-    let models = Account.app.models;
-    models.Role.findOne({name: user.userType}, function(err, role) {
-      if (err) throw err;
-      role.principals.create({
-        principalType: models.RoleMapping.USER,
-        principalId: user.id,
-      }, function(err, principal) {
-        if (err) throw err;
-        console.log('user has been added to role', principal);
-      });
-    });
     next();
   });
 };
