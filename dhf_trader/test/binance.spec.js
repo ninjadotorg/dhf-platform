e0576000318ef97041660b2a2b51668176b0a595
0xyeti <hoaibao@autonomous.nyc>
2018-09-10 17:53:36 +0700

    Remove unused

--- dhf_trader/test/binance.spec.js
+++ dhf_trader/test/binance.spec.js
@@ -61,38 +61,3 @@ describe('Binance', () => {
     sinon.assert.calledWithMatch(stubAPI.cancelOrder, params)
   }))
 })
-
-// async function test() {
-//     var gateway = new Gateway("binance", "test");
-//     await gateway.init()
-
-    // var params = {
-    //     symbol: "EDOETH",
-    //     quantity: "10",
-    //     price: "0.003112",
-    //     clientOrderId: "hihihi1"
-    // }
-    // var result = await gateway.action("buyLimit", params)
-    // console.log(result)
-
-
-    // params = {
-    //     symbol: "EDOETH",
-    //     clientOrderId: "hihihi1"
-    // }
-    // result = await gateway.action("getOrder", params)
-    // console.log(result)
-
-    // params = {
-    //     symbol: "ADAETH",
-    //     clientOrderId: "hihihi1"
-    // }
-    // result = await gateway.action("cancelOrder", params)
-    // console.log(result)
-
-
-//     let result = await gateway.action("getDepositAddress", {asset: "BTC"})
-//     console.log(result)
-// }
-//
-// test()
