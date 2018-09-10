d7993a9a7029a0761d11fc2bb1a9a1b8bf702782
0xyeti <hoaibao@autonomous.nyc>
2018-09-10 17:54:47 +0700

    Remove unused

--- dhf_trader/test/gateway.spec.js
+++ dhf_trader/test/gateway.spec.js
@@ -8,39 +8,3 @@ const Binance = require("../server/Exchanges/binance")
 
 describe('Gateway', () => {
 })
-
-// async function test() {
-//     var gateway = new Gateway("binance", "test");
-//     await gateway.init()
-
-    // var params = {
-    //     symbol: "EDOETH",
-    //     quantity: "10",
-    //     price: "0.003112",
-    //     clientOrderId: "hihihi1"
-    // }
-    // var result = await gateway.action("buyLimit", params)
-    // console.log(result)
-
-
-    // params = {
-    //     symbol: "EDOETH",
-    //     clientOrderId: "hihihi1"
-    // }
-    // result = await gateway.action("getOrder", params)
-    // console.log(result)
-
-    // params = {
-    //     symbol: "ADAETH",
-    //     clientOrderId: "hihihi1"
-    // }
-    // result = await gateway.action("cancelOrder", params)
-    // console.log(result)
-
-
-//     let result = await gateway.action("getDepositAddress", {asset: "BTC"})
-//     console.log(result)
-// }
-//
-// test()
-
