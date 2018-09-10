5dfe9ed87631f5a9a0e4bb15252f1bdb82692405
blackbean <tuananh@autonomous.nyc>
2018-09-10 11:14:03 +0700

    add check exist wallet

--- dhf_backend/common/models/link-to-wallet.js
+++ dhf_backend/common/models/link-to-wallet.js
@@ -2,45 +2,88 @@
 var async = require('async');
 
 module.exports = function(LinkToWallet) {
-  LinkToWallet.verify = function(authToken, verifyCode, userId, callback) {
-    LinkToWallet.findOne({
-      smartAddress: authToken,
+  LinkToWallet.verify = function(verifyCode, callback) {
+    LinkToWallet.findOne({where:{
+      verifyCode: verifyCode,
       status: 'pending',
-      userId: userId,
-    }, function(err, data) {
+    }}, function(err, data) {
       if (err) callback(err);
       let error = new Error();
       if (!data) {
         error.status = 404;
-        error.message = 'Token not found!';
+        error.message = 'Verify not found or code invalided!';
         return callback(error);
       }
-      if (data.verifyCode !== verifyCode) {
-        error.status = 401;
-        error.message = 'Verify code invalided';
-        return callback(error);
+
+      let createdDate = new Date(data.requestDate);
+      let now = new Date();
+      let validated = (now - createdDate) / 1000; // convert to  minutes;
+      const limiestTime = 1440; // one day (24hours) in a  minute
+      if (validated <= limiestTime) {
+        data.userId = LinkToWallet.app.currentUserId;
+        data.status = 'approved';
+        data.activeDate = new Date();
+        data.save(data, callback);
+      } else {
+        data.delete(data, function(err) {
+          if (err) callback(err);
+          error.status = 404;
+          error.message = 'Your active code has been expired!';
+          return callback(error);
+        });
       }
-      data.status = 'approved';
-      data.activeDate = new Date();
-      data.save(data, callback);
     });
   };
+  LinkToWallet.isLinked = function(walletId, callback) {
+    LinkToWallet.findOne({where:{
+        walletId: walletId
+    }}, function(err, data) {
+      if (err) callback(err);
+      callback(null, data !== null);
+    });
+  };
+  LinkToWallet.remoteMethod('isLinked', {
+    accepts: [
+      {arg: 'walletId', type: 'string'},
+    ],
+    returns: {arg: 'success', type: 'boolean'},
+    http: {path: '/has-linked', verb: 'get'},
+  });
+
   LinkToWallet.remoteMethod('verify', {
     accepts: [
-      {arg: 'authToken', type: 'string'},
       {arg: 'verifyCode', type: 'string'},
-      {arg: 'userId', type: 'string'},
     ],
     returns: {arg: 'success', type: 'boolean'},
     http: {path: '/verify', verb: 'post'},
   });
+
+  LinkToWallet.createVerifyCode = async function(){
+    let key = await Math.random().toString(36).substring(4);
+    return new Promise(function(resolve, reject){
+      LinkToWallet.findOne({where: {
+          verifyCode: key
+        }}, function (err, data) {
+        if(err) reject(err);
+        if(!data){
+          resolve(key)
+        } else {
+          resolve(LinkToWallet.createVerifyCode());
+        }
+      })
+    })
+  };
+
   LinkToWallet.observe('before save', function(ctx, next) {
     if (ctx.instance && ctx.isNewInstance) {
-      ctx.instance.status = 'pendding';
-      ctx.instance.requestDate = new Date();
-      ctx.instance.activeDate = new Date();
-      ctx.instance.verifyCode = Math.random().toString(36).substring(4);
-      next();
+      LinkToWallet.createVerifyCode().then(function (key) {
+        ctx.instance.status = 'pending';
+        ctx.instance.requestDate = new Date();
+        ctx.instance.activeDate = new Date();
+        ctx.instance.verifyCode = key;
+        next();
+      });
+
     } else {
       next();
     }
